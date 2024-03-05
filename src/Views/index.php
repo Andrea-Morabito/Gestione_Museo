74f517f69c03d23cd29e01de63f3e3bdf38f5b4c
Andrea-Morabito <andreamorabito05@gmail.com>
2024-03-05 11:47:53 +0100

    Fixed style.css in View\index.php

--- src/Views/index.php
+++ src/Views/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="<?php echo STYLE_PATH.'/style.css'?>">
+    <link rel="stylesheet" href="<?php echo '/ProgettoVenereUDA' . '/' . STYLE_PATH . '/style.css';?>">
 </head>
 <body>
     <h1><?php echo $nome." ".$cognome;

42cbbb36196d2468fedd2afe7662e7777028e4cf
Andrea-Morabito <andreamorabito05@gmail.com>
2024-03-17 12:06:45 +0100

    Updated database.sql

--- src/database.sql
+++ src/database.sql
@@ -45,3 +45,12 @@ CREATE TABLE accessori_biglietto
     FOREIGN KEY (Biglietto) REFERENCES biglietto(IdBiglietto),
     FOREIGN KEY (Accessorio) REFERENCES accessorio(IdAccessorio)
 );
+
+INSERT INTO `categoria` (`IdCategoria`, `descrizione`, `tipo`, `sconto`) VALUES
+(1, 'Sconto per clienti', 'cliente', 0),
+(2, 'Sconto per studenti', 'studente', 10),
+(3, 'Sconto per diversamente abili', 'diversamente_abile', 20);
+
+INSERT INTO `utente` (`IdUtente`, `email`, `nome`, `cognome`, `password`, `ruolo`, `categoria`) VALUES
+(1, 'admin@gmail.com', 'admin', 'admin', '$2y$10$wJWLjTQhVKGZg/sQY2yU5eN.lbSsljNLUKiTtnTaZmGfIZu189eYi', 'admin', 1),
+(2, 'utente@gmail.com', 'utente', ' utente', '$2y$10$BCx2y5QcJGqBgq.s5RJWBekJeSV6yj.wPBzKfP/lpwdZOmTOM6yZ.', 'user', 1);
